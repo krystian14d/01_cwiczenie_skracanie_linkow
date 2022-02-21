dda82c78b4571abf47e12d7423a40c59152c9925
Krystian <krystian14d@gmail.com>
2022-02-21 20:45:47 +0100

    caesar cipher implementation, application profiles added

---
+++ src/main/java/pl/javastart/dictionary/crypto/CaesarCipherService.java
@@ -0,0 +1,20 @@
+package pl.javastart.dictionary.crypto;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("prod")
+public class CaesarCipherService implements CipherService{
+    private static final int SHIFT = 3;
+
+    @Override
+    public String encrypt(String text) {
+        return null;
+    }
+
+    @Override
+    public String decrypt(String cipher) {
+        return null;
+    }
+}
---
+++ src/main/java/pl/javastart/dictionary/crypto/CipherService.java
@@ -0,0 +1,6 @@
+package pl.javastart.dictionary.crypto;
+
+public interface CipherService {
+    String encrypt(String text);
+    String decrypt (String cipher);
+}
